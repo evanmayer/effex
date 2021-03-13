21aed47b6242b8ec52eb88f69f3e3e0cec9e0f89
evanmayer <evanmayer@uchicago.edu>
2021-03-12 22:52:02 -0700

    Docstrings, refactor to add CLI

--- pfb_correlator.py
+++ pfb_correlator.py
@@ -5,7 +5,7 @@ The GPU is kept fed by popping sample chunks off of
 the deque, performing polyphase filter-bank preprocessing.
 Then the two streams are combined by cross-correlation.
 """
-
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -47,7 +47,7 @@ def spectrometer_poly(x, n_taps, n_branches):
     return x_psd
 
 
-def pfb_xcorr(gpu_iq_0, gpu_iq_1, total_lag, nfft=8192, mode='continuum'):
+def pfb_xcorr(gpu_iq_0, gpu_iq_1, total_lag, nfft, rate, fc, mode):
     '''Consume buffer data to compute PSDs in pairs and then cross-
     correlate them. Use mapped, pinned memory space allocated on the GPU.
     :param gpu_iq_0: cusignal mapped, pinned array of GPU memory containing SDR
@@ -56,9 +56,11 @@ def pfb_xcorr(gpu_iq_0, gpu_iq_1, total_lag, nfft=8192, mode='continuum'):
     data from channel 1
     :param total_lag: float, number of samples lag between channels 0 and 1.
     Calculated by sum of estimate_lag retvals.
-    :param nfft: int, number of fft bins to use in psd. Defaults to 8192.
+    :param nfft: int, number of fft bins to use in psd.
+    :param rate: float, SDR sample rate.
+    :param fc: float, SDR center tuning frequency
     :param mode: str, either 'continuum' for recording visibility amplitudes
-    with time, or 'spectrum' for recording spectram visibilities with time.
+    with time, or 'spectrum' for recording spectrum visibilities with time.
     Defaults to 'continuum'.
     :return: the result of one complex cross-correlation of the input IQ data.
     :rtype: If mode == 'continuum', float. If mode =='spectrum', cupy.array.
@@ -115,20 +117,22 @@ def pfb_xcorr(gpu_iq_0, gpu_iq_1, total_lag, nfft=8192, mode='continuum'):
     return vis
 
 
-def estimate_lag(iq_0, iq_1):
+def estimate_lag(iq_0, iq_1, rate, fc):
     '''Returns integer and fractional sample lag estimates. The sum is the 
     total estimated lag in samples between signals iq_0 and iq_1.
     :param iq_0: cusignal mapped, pinned array of GPU memory containing SDR
     data from channel 0
     :param iq_1: cusignal mapped, pinned array of GPU memory containing SDR
     data from channel 1
+    :param rate: float, SDR sample rate.
+    :param fc: float, SDR center tuning frequency
     :return: int, integer_lag, the integer lag between the argument signals in
     samples, and frac_lag, the fractional lag between the argument signals in
     fractions of a sample
     :rtype: tuple
     '''
     integer_lag = estimate_integer_lag(iq_0, iq_1)
-    frac_lag = estimate_fractional_lag(iq_0, iq_1, integer_lag)
+    frac_lag = estimate_fractional_lag(iq_0, iq_1, integer_lag, rate, fc)
 
     return integer_lag, frac_lag
 
@@ -158,7 +162,7 @@ def estimate_integer_lag(iq_0, iq_1):
     return integer_lag
 
 
-def estimate_fractional_lag(iq_0, iq_1, integer_lag):
+def estimate_fractional_lag(iq_0, iq_1, integer_lag, rate, fc):
     '''Returns fractional sample lag estimate in samples between signals iq_0
     and iq_1. First corrects integer sample lag to make estimating the
     fractional lag tractable, then finds the slope of the phase of the cross-
@@ -167,6 +171,9 @@ def estimate_fractional_lag(iq_0, iq_1, integer_lag):
     data from channel 0
     :param iq_1: cusignal mapped, pinned array of GPU memory containing
     SDR data from channel 1
+    :param integer_lag: int, estimated value from estimate_integer_lag()
+    :param rate: float, SDR sample rate.
+    :param fc: float, SDR center tuning frequency
     :return: float, frac_lag, the fractional lag between the argument signals
     in samples
     :rtype: float
@@ -237,14 +244,15 @@ async def streaming(sdr, buf, num_samp, start_time, run_time):
     except Exception as exc:
         print('streaming call generated an exception: %s' % (exc))
         raise exc
-    await sdr.stop()
+    finally:
+        await sdr.stop()
                                                                                                                
     print('Buffering ended at {}'.format(time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime(time.time()))))
 
     return
 
 
-def process_iq(buf_0, buf_1, num_samp, nfft, start_time, run_time, mode):
+def process_iq(buf_0, buf_1, num_samp, nfft, rate, fc, start_time, run_time, mode):
     '''This is the main function of the correlator. It holds on to two
     multiprocessing.Queue() instances, one for each SDR channel, and handles
     taking chunks of num_samp IQ samples off of each buffer and sending them to
@@ -257,13 +265,15 @@ def process_iq(buf_0, buf_1, num_samp, nfft, start_time, run_time, mode):
     :param buf_1: multiprocessing.Queue() instance
     :param num_samp: int, number of samples to read async from sdr at a time.
     2^18 works well for RTL-SDRblog v3 dongles.
-    :param nfft: int, number of fft bins to use in psd. Defaults to 8192.
+    :param nfft: int, number of fft bins to use in psd.
+    :param rate: float, SDR sample rate.
+    :param fc: float, SDR center tuning frequency
     :param start_time: float, time in ms since UNIX epoch to begin streaming
     async samples. Helps multiple streaming processes start closer to the same time.
     :param run_time: float, time in ms since UNIX epoch to end streaming async
     samples.
     :param mode: str, either 'continuum' for recording visibility amplitudes
-    with time, or 'spectrum' for recording spectram visibilities with time.
+    with time, or 'spectrum' for recording spectrum visibilities with time.
     Defaults to 'continuum'.
     :return: vis_out, a list of either floats or cupy.arrays, depending on mode.
     '''
@@ -299,40 +309,47 @@ def process_iq(buf_0, buf_1, num_samp, nfft, start_time, run_time, mode):
             # used as input on first cycle.
             # Estimate integer and fractional sample delays
             if first_time:
-                integer_lag, frac_lag = estimate_lag(gpu_iq_0, gpu_iq_1)
+                integer_lag, frac_lag = estimate_lag(gpu_iq_0, gpu_iq_1, rate, fc)
                 total_lag = integer_lag + frac_lag
                 print('Estimated lag (samples): {} + {}'.format(integer_lag, frac_lag))
-                total_lag -= 1
+                total_lag -= 1 # Start sweep a full sample offset from "phased-up"
             # Provide a rudimentary method of sweeping phase in time: increase
-            # lag in samples
+            # lag in samples. This is more of a dev/testing feature.
             elif sweep_step:
                 total_lag += sweep_step
                 print('Estimated lag (samples): {}'.format(float(total_lag)))
 
-            visibility = pfb_xcorr(gpu_iq_0, gpu_iq_1, total_lag, nfft=nfft, mode=mode)
+            visibility = pfb_xcorr(gpu_iq_0, gpu_iq_1, total_lag, nfft, rate, fc, mode)
             vis_out.append(visibility)
             first_time = False
 
     return vis_out
 
 
-def post_process(raw_output, mode):
+def post_process(raw_output, rate, fc, nfft, num_samp, mode):
     '''Handles saving and displaying data.
     :param raw_output: python list, if mode 'continuum', a list of visibility
     amplitudes, if mode 'spectrum', a list of cupy arrays, each
     one being a complex visibility spectrum from a pair of SDR
-    buffer reads.
+    buffer reads
+    :param rate: float, SDR sample rate.
+    :param fc: float, SDR center tuning frequency
+    :param nfft: int, number of fft bins to use in psd.
+    :param num_samp: int, number of samples to read async from sdr at a time.
     :param mode: str, either 'continuum' for recording visibility amplitudes
-    with time, or 'spectrum' for recording spectram visibilities with time.
+    with time, or 'spectrum' for recording spectrum visibilities with time.
     Defaults to 'continuum'.
     :return: fname, the filename to which output processed data is written
     :rtype: str
     '''
-    def record_visibilities(visibilities, mode):
+    def record_visibilities(visibilities, fc, mode):
         '''
-        Inputs:
-        - visibilites: ndim cupy array
-        - mode: ['continuum', 'spectrum']
+        :param visibilites: ndim cupy array
+        :param mode: str, either 'continuum' for recording visibility amplitudes
+        with time, or 'spectrum' for recording spectrum visibilities with time.
+        Defaults to 'continuum'.
+	:return: str, fname, filename that was written to
+	:rtype: str
         '''
         fname = time.strftime('visibilities_%Y%m%d-%H%M%S')+'.csv'             
         
@@ -349,11 +366,11 @@ def post_process(raw_output, mode):
         return fname
 
 
-    def visualize(visibilities, mode):
+    def visualize(visibilities, rate, nfft, num_samp, mode):
         '''Handles plotting 1D continuum data or 2D spectrum data with respect to time.
         :param visibilites: ndim cupy array, output of correlator function pfb_xcorr
         :param mode: str, either 'continuum' for recording visibility amplitudes
-        with time, or 'spectrum' for recording spectram visibilities with time.
+        with time, or 'spectrum' for recording spectrum visibilities with time.
         Defaults to 'continuum'.
         '''
         amp = cp.asnumpy(cp.sqrt(cp.real(visibilities * cp.conj(visibilities))))
@@ -389,8 +406,8 @@ def post_process(raw_output, mode):
             im01 = axes[0][1].plot(x, real_part, label='real part')
             im01 = axes[0][1].plot(x, imag_part, alpha=0.5, label='imag_part')
             axes[0][1].set_xlabel(xlabel)
-            axes[0][1].set_ylabel('Re[xcorr]')
-            axes[0][1].set_title('Complex Cross-Correlation Re{}')
+            axes[0][1].set_ylabel('Amplitude')
+            axes[0][1].set_title('Complex Cross-Correlation Real & Imag')
             axes[0][1].legend(loc='best')
             
             im10 = axes[1][0].plot(x, phase)
@@ -401,7 +418,7 @@ def post_process(raw_output, mode):
             im11 = axes[1][1].plot(x, imag_part, label='imag_part')
             axes[1][1].set_xlabel(xlabel)
             axes[1][1].set_ylabel('Amplitude')
-            axes[1][1].set_title('Complex Cross-Correlation imag')
+            axes[1][1].set_title('Complex Cross-Correlation Imag')
         else:
             freqs = fc + np.fft.fftshift(np.fft.fftfreq(nfft, d=1/rate))
             num_spectra = np.array(range(visibilities.shape[0]))
@@ -439,12 +456,11 @@ def post_process(raw_output, mode):
 
     # Convert list to cupy array
     visibilities = cp.array(raw_output)
-    print('Processed', len(visibilities) * 2 * num_samp * iq_0.itemsize / run_time, 'Bytes/sec')
 
-    fname = record_visibilities(visibilities, mode)
+    fname = record_visibilities(visibilities, fc, mode)
     print('Data recorded to {}.'.format(fname))
 
-    visualize(visibilities, mode)
+    visualize(visibilities, rate, nfft, num_samp, mode)
 
     return
 
@@ -452,23 +468,46 @@ def post_process(raw_output, mode):
 
 if __name__ == "__main__":
     # -------------------------------------------------------------------------
+    # CONSTANTS 
+    # -------------------------------------------------------------------------
+    d_len = int(5e8 // (2**18 * np.dtype(np.complex128).itemsize) // 2) # sized for 4GB RAM on NVIDIA Jetson Nano
+    streaming_fudge_factor = 1. # sec, allow some time for streaming subprocesses to get to starting line
+
+    # -------------------------------------------------------------------------
+    # ARGUMENT PARSING
+    # -------------------------------------------------------------------------
+    parser = argparse.ArgumentParser(description='GPU FX correlator accelerated with NVIDIA cuSignal.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--time',       '-T', default=1, type=float, dest='run_time',
+        help='(sec) Total amount of time to run correlator.')
+    parser.add_argument('--bandwidth',  '-B', default=2.4e6, type=float, dest='rate',
+        help='(Hz) Receiver bandwidth. For RTL-SDR dongles, this determines sample rate. Applied to both channels.')
+    parser.add_argument('--frequency',  '-F', default=1.4204e9, type=float, dest='fc',
+        help='(Hz) Receiver center tuning frequency. Applied to both channels.')
+    parser.add_argument('--num_samp',   '-N', default=2**18, type=int, dest='num_samp',
+        help='(int) Number of samples to read from SDR on each call. Large powers of 2 less than 2**18 are recommended for RTL-SDRs.')
+    parser.add_argument('--resolution', '-R', default=2**12, type=int, dest='nfft',
+        help='(int) Number of FFT bins to use in processing and plotting.')
+    parser.add_argument('--gain',       '-G', default=49.6, type=float, dest='gain',
+        help='(dB) Tuner gain in Decibels. Tuner gain has an impact on receiver sensitivity and may affect clock stability due to heat generation. Applied to both channels.')
+    parser.add_argument('--mode',       '-M', default='spectrum', type=str, choices=['continuum', 'spectrum'], dest='mode', 
+        help='(str) Choose one: continuum mode estimates visibility amplitude over time, throwing away phase and frequency information. Spectrum mode keeps complex visibilities. Affects data memory usage, visualization speed, and output file size.')
+
+    args = parser.parse_args()
+
+    # -------------------------------------------------------------------------
     # SDR INIT
     # -------------------------------------------------------------------------
-    num_samp = 2**18
-    rate = 2.2e6
-    fc = 1420.4e6 # Frequency of interest
-    gain = 49.6
-    
     # Dithering depends on evanmayer's fork of roger-'s pyrtlsdr and keenerd's
     # experimental fork of librtlsdr
     sdr_0 = RtlSdr(device_index=0, dithering_enabled=False)
     sdr_1 = RtlSdr(device_index=1, dithering_enabled=False)
-    sdr_0.rs = rate
-    sdr_1.rs = rate
-    sdr_0.fc = fc
-    sdr_1.fc = fc
-    sdr_0.gain = gain
-    sdr_1.gain = gain
+    sdr_0.rs = args.rate
+    sdr_1.rs = args.rate
+    sdr_0.fc = args.fc
+    sdr_1.fc = args.fc
+    sdr_0.gain = args.gain
+    sdr_1.gain = args.gain
     
     # -------------------------------------------------------------------------
     # CPU & GPU MEMORY SETUP
@@ -477,41 +516,42 @@ if __name__ == "__main__":
     iq_1 = np.array([], dtype=np.complex128)
 
     # Store sample chunks in 2 deques
-    d_len = int(5e8 // (num_samp * iq_0.itemsize) // 2)
     buf_0 = multiprocessing.Queue(d_len)
     buf_1 = multiprocessing.Queue(d_len)
-
-    # FFT Frequency bin resolution
-    nfft = 2**12
 
     # -------------------------------------------------------------------------
     # RUN 
     # -------------------------------------------------------------------------
-    # ['spectrum', 'continuum']
-    mode = 'spectrum'
-    run_time = 1
     sweep_step = 0.0 # 2e-4 # of samples to add to signal delay on each xcorr to sweep across delay space
-    start_time = time.time() + 1 # Give streaming processes a second to get to to the starting line
+
+    start_time = time.time() + streaming_fudge_factor
     print('Interferometry begins at {}'.format(time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime(start_time))))
     # IQ source processes
-    proc_0 = streaming(sdr_0, buf_0, num_samp, start_time, run_time)
+    # ECM: FIXME:
+    # streaming() is an async function, so this will throw a warning about 
+    # not awaiting it, but of course it's being run by asyncio.run, just not
+    # here. There might be another way to do this, but this works for now.
+    proc_0 = streaming(sdr_0, buf_0, args.num_samp, start_time, args.run_time)
     producer_0 = multiprocessing.Process(target=asyncio.run, args=(proc_0,))
-    proc_1 = streaming(sdr_1, buf_1, num_samp, start_time, run_time)
+    proc_1 = streaming(sdr_1, buf_1, args.num_samp, start_time, args.run_time)
     producer_1 = multiprocessing.Process(target=asyncio.run, args=(proc_1,))
     producer_0.start()
     producer_1.start()
     
     # IQ sink
     raw_output = process_iq(buf_0, buf_1,
-                            num_samp,
-                            nfft,
-                            start_time, run_time,
-                            mode=mode)
+                            args.num_samp,
+                            args.nfft,
+			    args.rate,
+			    args.fc,
+                            start_time, args.run_time,
+                            mode=args.mode)
 
     print('IQ processing complete, buffers drained.')
+
     sdr_0.close()
     sdr_1.close()
     print('SDRs closed.')
 
-    post_process(raw_output, mode)
+    post_process(raw_output, args.rate, args.fc, args.nfft, args.num_samp, args.mode)
 
