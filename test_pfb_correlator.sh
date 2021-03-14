f3614d2c7a5261aa9f549446846571e6570db67e
evanmayer <evanmayer@uchicago.edu>
2021-03-14 14:31:03 -0700

    folder restructure 2

--- test_pfb_correlator.sh
+++ test_pfb_correlator.sh
@@ -2,7 +2,9 @@
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 1 &&
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 1 &&
 
-python pfb_correlator.py
+# python pfb_correlator.py
+
+python python/pfb_correlator.py --time 60 --bandwidth 2.4e6 --frequency 91.3e6 --num_samp 262144 --resolution 4096 --gain 49.6 --mode spectrum
 
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 0 &&
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 0;
