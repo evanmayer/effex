a66720c46aef9318300ed55be977cc44208dc5ed
evanmayer <evanmayer@uchicago.edu>
2021-03-21 23:06:00 -0700

    bias-t commands were wrong

--- run_pfb_correlator.sh
+++ run_pfb_correlator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 1 &&
-~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 1 &&
+~/github/rtl-sdr-blog/build/src/rtl_biast -d 1 -b 1 &&
 
 # python pfb_correlator.py
 
-python effex/pfb_correlator.py --time 6 --bandwidth 2.4e6 --frequency 91.3e6 --num_samp 262144 --resolution 4096 --gain 49.6 --mode spectrum
+python effex/pfb_correlator.py --time 1 --bandwidth 2.4e6 --frequency 1.4204e9 --num_samp 262144 --resolution 4096 --gain 49.6 --mode spectrum
 
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 0 &&
-~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 0;
+~/github/rtl-sdr-blog/build/src/rtl_biast -d 1 -b 0;
