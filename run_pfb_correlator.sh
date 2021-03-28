92c6a84870a980553e2c26edfe8b8565dc3c1f0b
evanmayer <evanmayer@uchicago.edu>
2021-03-27 21:59:42 -0700

    Updates to example run script

--- run_pfb_correlator.sh
+++ run_pfb_correlator.sh
@@ -2,8 +2,6 @@
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 1 &&
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 1 -b 1 &&
 
-# python pfb_correlator.py
-
 python effex/pfb_correlator.py --time 1 --bandwidth 2.4e6 --frequency 1.4204e9 --num_samp 262144 --resolution 4096 --gain 49.6 --mode spectrum
 
 ~/github/rtl-sdr-blog/build/src/rtl_biast -d 0 -b 0 &&
